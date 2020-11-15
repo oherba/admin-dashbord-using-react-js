db7837865b82b33662b06b865fb46978d476b60d
oherba <44850388+oherba@users.noreply.github.com>
2020-11-15 18:32:38 +0100

    send request to the server

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,68 +2,45 @@ import './Login.css'
 import React, { useState } from 'react';
 import axios from 'axios';
 
+    function login(email,pass)
+    {
+        axios.post('http://10.1.33.255:8080/api/login', {
+            email: email,
+            password: pass
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
 
-function Login(props) {
+    function click(){
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value
+        validateInput(email, password, (err) => {
+            if (err.email || err.password)
+            {
+                // if (err.password)
 
-
-    const sendDetailsToServer = () => {
-        if({email} && {password}) {
-            props.showError(null);
-            const payload={
-                "email":{email},
-                "password":{password},
+                // if (err.email)
+                console.log("ïnvalid input")
+                return
             }
-        //     axios.post(API_BASE_URL+'/user/register', payload)
-        //         .then(function (response) {
-        //             if(response.status === 200){
-        //                 setState(prevState => ({
-        //                     ...prevState,
-        //                     'successMessage' : 'Registration successful. Redirecting to home page..'
-        //                 }))
-        //                 localStorage.setItem(ACCESS_TOKEN_NAME,response.data.token);
-        //                 redirectToHome();
-        //                 props.showError(null)
-        //             } else{
-        //                 props.showError("Some error ocurred");
-        //             }
-        //         })
-        //         .catch(function (error) {
-        //             console.log(error);
-        //         });    
-        // } else {
-        //     props.showError('Please enter valid email and password')    
-         }   
-    }
-    
-    const validate = e => {
-        
-      }
-    
-    
-
-    const useFormInput = initialValue => {
-        const [value, setValue] = useState(initialValue);
-       
-        const handleChange = e => {
-          setValue(e.target.value);
-        }
-        return {
-          value,
-          onChange: handleChange
-        }
-      }
-      
-      const handleSubmitClick = (e) => {
-        e.preventDefault();
-            console.log("email is" + {email} + "      ");
-            console.log("pasword is" + {password} + "      ");
-            validate();
-            // sendDetailsToServer()    
+            login(email, password)
+        })
     }
 
-    const email = useFormInput('');
-    const password = useFormInput('');
+    function validateInput(email, password, callback)
+    {
+        const passerr = password.length < 5
+        const emailerr = email.length < 5
+        return (callback({email:emailerr,password:passerr}))
+    }
+function Login(props) {
     
+
     return (
         <div className="base-container">
             <div className="split right ">
@@ -75,19 +52,19 @@ function Login(props) {
                 <div className= " centered">
                     <div className="header">Administarteur System</div>
                     <div className="label-div">
-                        <label className="email"   htmlFor="email" value={email}>Email Address</label>
+                        <label className="email"   htmlFor="email" >Email Address</label>
                     </div>
-                    <input className="emailInput" type="text" name="email"></input>
+                    <input className="emailInput" id="email" type="text" name="email"></input>
                     <div className="label-div">
                         <label  className="password" htmlFor="password">Password</label>
                     </div>
-                    <input className="passwordInput" type="text" name="password" value={password} ></input>
+                    <input className="passwordInput" id="password" type="password" ></input>
                     <div className="checkBox">
                         <input  type="checkbox"  id="checkbox" name="remember"></input>
                         <label htmlFor="checkbox" >Remember me</label>
                     </div>
                     <div className="btn-div">
-                        <button  className="sublog" type="button" onClick={handleSubmitClick} >Login</button>
+                        <button  className="sublog" type="button" onClick={click} >Login</button>
                     </div>
                     
                 </div>
