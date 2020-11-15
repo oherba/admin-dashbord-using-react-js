211035840610f05569e2aac3afb84149b1993610
oherba <othmanherba7@gmail.com>
2020-11-15 05:51:52 -0700

    working on login authentification

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,67 @@
 import './Login.css'
 import React, { useState } from 'react';
+import axios from 'axios';
 
 
 function Login(props) {
-    const username = useFormInput('');
+
+
+    const sendDetailsToServer = () => {
+        if({email} && {password}) {
+            props.showError(null);
+            const payload={
+                "email":{email},
+                "password":{password},
+            }
+        //     axios.post(API_BASE_URL+'/user/register', payload)
+        //         .then(function (response) {
+        //             if(response.status === 200){
+        //                 setState(prevState => ({
+        //                     ...prevState,
+        //                     'successMessage' : 'Registration successful. Redirecting to home page..'
+        //                 }))
+        //                 localStorage.setItem(ACCESS_TOKEN_NAME,response.data.token);
+        //                 redirectToHome();
+        //                 props.showError(null)
+        //             } else{
+        //                 props.showError("Some error ocurred");
+        //             }
+        //         })
+        //         .catch(function (error) {
+        //             console.log(error);
+        //         });    
+        // } else {
+        //     props.showError('Please enter valid email and password')    
+         }   
+    }
+    
+    const validate = e => {
+        
+      }
+    
+    
+
+    const useFormInput = initialValue => {
+        const [value, setValue] = useState(initialValue);
+       
+        const handleChange = e => {
+          setValue(e.target.value);
+        }
+        return {
+          value,
+          onChange: handleChange
+        }
+      }
+      
+      const handleSubmitClick = (e) => {
+        e.preventDefault();
+            console.log("email is" + {email} + "      ");
+            console.log("pasword is" + {password} + "      ");
+            validate();
+            // sendDetailsToServer()    
+    }
+
+    const email = useFormInput('');
     const password = useFormInput('');
     
     return (
@@ -17,13 +75,13 @@ function Login(props) {
                 <div className= " centered">
                     <div className="header">Administarteur System</div>
                     <div className="label-div">
-                        <label className="email" {...username}  htmlFor="username">Email Address</label>
+                        <label className="email"   htmlFor="email" value={email}>Email Address</label>
                     </div>
-                    <input className="emailInput" type="text" name="username"></input>
+                    <input className="emailInput" type="text" name="email"></input>
                     <div className="label-div">
-                        <label  className="password" {...password} htmlFor="password">Password</label>
+                        <label  className="password" htmlFor="password">Password</label>
                     </div>
-                    <input className="passwordInput" type="text" name="password" ></input>
+                    <input className="passwordInput" type="text" name="password" value={password} ></input>
                     <div className="checkBox">
                         <input  type="checkbox"  id="checkbox" name="remember"></input>
                         <label htmlFor="checkbox" >Remember me</label>
@@ -36,30 +94,11 @@ function Login(props) {
             </div>
         </div>
     )
-}
-
-const validate = e => {
-    
-  }
+// }
 
 
 
-const useFormInput = initialValue => {
-    const [value, setValue] = useState(initialValue);
-   
-    const handleChange = e => {
-      setValue(e.target.value);
-    }
-    return {
-      value,
-      onChange: handleChange
-    }
-  }
-  
-  const handleSubmitClick = (e) => {
-    e.preventDefault();
-        console.log("anan banana");
-        validate();
-        // sendDetailsToServer()    
+
+
 }
 export default Login
