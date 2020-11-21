8434d2a085f2156de568953b7c60197a59040308
Othman Herba <oherba@e1r7p1.1337.ma>
2020-11-21 20:50:55 +0100

    working on dashboard

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import Index from './components/login/index'
 import './App.css';
 import Login from './components/login/Login.jsx';
 import Loggedin from './components/login/Loggedin.jsx';
+import Dashboard from './components/login/Dashboard';
+import Dashbd from './components/login/Dashboard';
 
 function App() {
     return (
       <div className="App">
-        <Loggedin/>
+       <Dashbd></Dashbd>
+        {/* <Loggedin/> */}
       </div>
     );
 }
 
 export default App;
+
+
---
+++ src/components/login/Dashboard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import './Dashboard.css'
+
+function Dashbd() {
+  return (<div className="blan">
+        <table>
+            <tr>
+            <th>image</th>
+            <th>NOM</th>
+            <th>Prenom</th>
+            <th>Service</th>
+            <th>Numero</th>
+            <th>Reglage</th>
+            <th>Editer</th>
+            <th>suprimer</th>
+            </tr>
+            <tr>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            </tr>
+            <tr>
+            <td>Eve</td>
+            <td>Jackson</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            <td>94</td>
+            
+            </tr>
+            <tr>
+            <td>Adam</td>
+            <td>Johnson</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            <td>67</td>
+            </tr>
+        </table>
+        </div>);
+}
+
+export default Dashbd;
